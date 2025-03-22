81fa9503f5e1eb84dd9758bc334156054172bc55
forele7 <mar_vov2008@mail.ru>
2025-03-22 16:31:34 +0300

    Create Void Main

--- Vovchenko_LR3_project.cpp
+++ Vovchenko_LR3_project.cpp
@@ -10,14 +10,14 @@
 using namespace std;
 
 //число А
-int a;
-void setA(int a);
-int getA() {return a;};
+double a;
+void setA(double a);
+double getA() {return a;};
 
 //число В
-int b;
-void setB(int b);
-int getB() {return b;};
+double b;
+void setB(double b);
+double getB() {return b;};
 
 
 bool git (string input)
@@ -41,7 +41,7 @@ bool git (string input)
     return true;
 }
 
-function<void()> Enter_Number(int& varLink, string label)
+function<void()> Enter_Number(double& varLink, string label)
 {
     return [&varLink, label]()
     {
@@ -53,7 +53,7 @@ function<void()> Enter_Number(int& varLink, string label)
             cout << label << "= ";
             getline(cin, get_input);
         }
-        varLink = stoi(get_input);
+        varLink = stod(get_input);
     };
 }
 
@@ -67,31 +67,32 @@ struct Menu1
 //задание 1
 void ShowAnum()
 {
-    Enter_Number(a, "Enter number A: ")();
-    cout << a << endl;
+    // Enter_Number(a, "Enter number A: ")();
+    // cout << a << endl;
 }
 
 //задание 2
-double ShowB()
+void ShowBnum()
 {
-    Enter_Number(b, "Enter number B: ")();
-    cout << b << endl;
+    // Enter_Number(b, "Enter number B: ")();
+    // cout << b << endl;
 }
 
-//задание 3
-double Operation1(int a = 5, int b = 7)
+// //задание 3
+void Operation1()
 {
-    //int a = 5;
-    //int b = 7;
-    //return a*b;
+    // Enter_Number(a, "Enter number A: ")();
+    // Enter_Number(b, "Enter number B: ")();
+    // cout << "A*B = " << a*b << endl;
 }
 
-// задание 3
-double Operation2()
+// // задание 4
+void Operation2()
 {
-    //int a = 5;
-    //int b = 7;
-    //return a/b;
+    // Enter_Number(a, "Enter number A: ")();
+    // Enter_Number(b, "Enter number B: ")();
+    // if (b == 0){cout << "На ноль делить нельзя!";}
+    // else {cout << "A/B = " << a/b << endl;}
 }
 
 int main()
@@ -99,12 +100,12 @@ int main()
     map<int, Menu1> menu =
     {
         {1, {"Вывести A", ShowAnum}},
-        {2, {"Вывести B", ShowB}},
+        {2, {"Вывести B", ShowBnum}},
         {3, {"Выполнить операцию “*”", Operation1}},
         {4, {"Выполнить операцию “/”", Operation2}},
     };
 
-    int choice = 0;
+    double choice = 0;
 
     cout << "Menu: " << endl;
     for (const auto& item : menu)
@@ -116,7 +117,7 @@ int main()
 
     while (true)
     {
-    Enter_Number(choice, "Enter Number: ")();
+    Enter_Number(choice, "Enter Number of task: ")();
     if (choice == 0)
     {
         cout << endl;
